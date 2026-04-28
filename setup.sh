b69883b6389af959a6a7eb2e4996e31b19e2d318
Daniel Yang <60305796+danielwzyang@users.noreply.github.com>
2026-04-28 14:22:24 -0400

    add setup script

---
+++ setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+ln -sf ~/Documents/dotfiles/.vimrc ~/.vimrc
+ln -sf ~/Documents/dotfiles/.zshrc ~/.zshrc
+ln -sf ~/Documents/dotfiles/.gitconfig ~/.gitconfig
+ln -sf ~/Documents/dotfiles/daniel.zsh-theme ~/.oh-my-zsh/custom/themes/daniel.zsh-theme
