f6a82bb7b0d5bacdeab945d5e8ffd74946573c03
Daniel Yang <60305796+danielwzyang@users.noreply.github.com>
2026-08-13 21:50:20 -0400

    feat(setup): update setup w zsh and omz install + chsh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+set -e # end on fail
+
+sudo dnf install -y zsh
+
+RUNZSH=no KEEP_ZSHRC=yes sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+mkdir -p ~/.oh-my-zsh/custom/themes
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/dotfiles/.gitconfig ~/.gitconfig
 ln -sf ~/dotfiles/daniel.zsh-theme ~/.oh-my-zsh/custom/themes/daniel.zsh-theme
+
+chsh -s $(command -v zsh)
